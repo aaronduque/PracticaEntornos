e65a27ad8baaf5348fa82aba382e197cdd5397dc
Pedro <Pedro@Lenovo-Pedro.home>
2019-05-23 17:33:22 +0200

    Creacion CuentaBancaria

--- PracticaEntornos/src/practicaentornos/CuentaBancaria.java
+++ PracticaEntornos/src/practicaentornos/CuentaBancaria.java
@@ -7,8 +7,44 @@ package practicaentornos;
 
 /**
  *
- * @author Aaron
+ * @author Pedro
  */
 public class CuentaBancaria {
+    String nombre;
+    Cliente propietario;
+    int saldoActual;
+    int codigoCuenta;
+    
+    public CuentaBancaria(){
+        
+    }
+
+    public CuentaBancaria(String nombre, Cliente propietario, int saldoActual, int codigoCuenta) {
+        this.nombre = nombre;
+        this.propietario = propietario;
+        this.saldoActual = saldoActual;
+        this.codigoCuenta = codigoCuenta;
+    }
+
+    
+    public void ingresarSaldo(int cantidad){
+        int cantidadActual = getSaldoActual();
+        
+        setSaldoActual(cantidadActual + cantidad);
+    }
+    public void retirarSaldo(int cantidad){
+        int cantidadActual = getSaldoActual();
+        
+        setSaldoActual(cantidadActual - cantidad);
+    }
+
+    public int getSaldoActual() {
+        return saldoActual;
+    }
+
+    public void setSaldoActual(int saldoActual) {
+        this.saldoActual = saldoActual;
+    }
+    
     
 }
