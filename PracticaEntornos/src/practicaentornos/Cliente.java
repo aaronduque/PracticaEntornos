bff1ab55fc11c3011c3784f4c2c82c820425ce53
Aaron <Aaron@192.168.1.103>
2019-05-23 18:21:13 +0200

    push version de la rama cliente v.2

--- PracticaEntornos/src/practicaentornos/Cliente.java
+++ PracticaEntornos/src/practicaentornos/Cliente.java
@@ -9,13 +9,14 @@ package practicaentornos;
  *
  * @author Aaron
  */
+
 public class Cliente {
-    String nombre;
-    String apellidos;
-    int edad;
-    String direccion;
-    int telefono;
-    int codigoCliente;
+    private String nombre;
+    private String apellidos;
+    private int edad;
+    private String direccion;
+    private int telefono;
+    private int codigoCliente;
     
     public Cliente(){
         
@@ -28,6 +29,55 @@ public class Cliente {
         this.telefono = telefono;
         this.codigoCliente = codigoCliente;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(int codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+    
 
     @Override
     public String toString() {
